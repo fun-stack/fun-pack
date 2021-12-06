d69b4e2faef01fddee5c50bafab88536b9e1576e
johannes karoff <johannes@karoff.net>
2021-12-06 17:24:28 +0100

    add lambda dev

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const {webDev} = require("./webpack.config.web.dev");
 const {webProd} = require("./webpack.config.web.prod");
+const {lambdaDev} = require("./webpack.config.lambda.dev");
 const {lambdaProd} = require("./webpack.config.lambda.prod");
 
-module.exports = {webDev, webProd, lambdaProd};
+module.exports = {webDev, webProd, lambdaDev, lambdaProd};
---
+++ src/webpack.config.lambda.dev.js
@@ -0,0 +1,35 @@
+const Path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {merge} = require("webpack-merge");
+
+//TODO?!
+const wd = Path.join(__dirname, "../../../..");
+
+const rootPath = Path.resolve(wd, "../../../..");
+const distDir = Path.join(wd, "dev");
+
+function dev() {
+  return merge(require(Path.resolve(wd, "scalajs.webpack.config")), {
+    resolve: {
+      modules: [rootPath, wd, Path.join(wd, "node_modules")],
+    },
+    plugins: [
+      new CleanPlugin(distDir),
+      // to analyze bundle size, have a look at these:
+      // new BundleAnalyzerPlugin({analyzerMode: "server"}),
+      // https://www.npmjs.com/package/source-map-explorer
+    ],
+    optimization: {
+      minimize: false,
+    },
+    target: 'node',
+    output: {
+      path: distDir,
+      filename: "main.js",
+      libraryTarget: 'umd'
+    },
+  });
+}
+
+module.exports = {lambdaDev: dev};
