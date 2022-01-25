f54329cd9698bc40fd4087f13b03e5221348ffe0
johannes karoff <johannes@karoff.net>
2022-01-25 11:08:34 +0100

    add extraStaticDirs to web dev

--- src/webpack.config.web.dev.js
+++ src/webpack.config.web.dev.js
@@ -17,12 +17,20 @@ function dev(argsRaw) {
   const args = Object.assign({
     indexHtml: "src/main/html/index.html",
     assetsDir: "assets",
-    extraWatchDirs: []
+    extraWatchDirs: [],
+    extraStaticDirs: []
   }, argsRaw);
 
   const indexHtml = Path.resolve(rootPath, args.indexHtml);
   const assetsDir = Path.join(rootPath, args.assetsDir);
   const extraWatchDirs = args.extraWatchDirs.map(path => Path.join(rootPath, path));
+  //TODO: switch to webpack dev server 4, then we do not need this rewrites/ignore workaround for static assets.
+  const extraStaticDirs = args.extraStaticDirs.map(path => {
+    return {
+      path: Path.join(rootPath, path),
+      dir: path
+    };
+  });
 
   const staticCopyFiles = [
     glob.sync(Path.join(wd, "*-fastopt-loader.js")),
@@ -60,13 +68,22 @@ function dev(argsRaw) {
       ],
     },
     devServer: {
-      contentBase: [devDir, assetsDir].concat(extraWatchDirs),
+      contentBase: [devDir, assetsDir].concat(extraWatchDirs).concat(extraStaticDirs.map(x => x.path)),
       allowedHosts: [".localhost"],
       disableHostCheck: false,
       compress: false,
       watchContentBase: true,
       watchOptions: {
-        ignored: (f) => f.endsWith(".tmp"),
+        ignored: (f) => f.endsWith(".tmp") || extraStaticDirs.some(d => f.startsWith(d.path))
+      },
+      historyApiFallback: {
+        rewrites: extraStaticDirs.map(d => {
+          const regex = new RegExp(`^/${d.dir}/`, '');
+          return {
+            from: regex,
+            to: context => context.parsedUrl.pathname.replace(regex, '')
+          };
+        })
       },
       // writeToDisk: true,
       hot: false,
