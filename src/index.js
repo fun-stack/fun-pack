c76941d3e439b36ac4ec32154036e9db7c32fdab
johannes karoff <johannes@karoff.net>
2022-09-18 09:26:25 +0200

    add binProd helper

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const {webDev} = require("./webpack.config.web.dev");
 const {webProd} = require("./webpack.config.web.prod");
 const {lambdaDev} = require("./webpack.config.lambda.dev");
 const {lambdaProd} = require("./webpack.config.lambda.prod");
+const {binProd} = require("./webpack.config.bin.prod");
 
-module.exports = {webDev, webProd, lambdaDev, lambdaProd};
+module.exports = {webDev, webProd, lambdaDev, lambdaProd, binProd};
---
+++ src/webpack.config.bin.prod.js
@@ -0,0 +1,41 @@
+const {baseConfig} = require("./webpack.base.js");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const webpack = require('webpack');
+const Path = require('path');
+const fs = require('fs');
+const {merge} = require("webpack-merge");
+
+const wd = Path.resolve(Path.dirname(module.parent.parent.filename));
+
+function prod(argsRaw) {
+  process.env.NODE_ENV = "production";
+
+  const args = Object.assign({
+    fileName: "index.js",
+    outputDir: "dist",
+  }, argsRaw);
+
+  const outputDir = Path.join(wd, args.outputDir);
+  const fileName = args.fileName
+
+  return merge(baseConfig(wd, args.entrypoint), {
+    mode: "production",
+    target: "node",
+    output: {
+      path: outputDir,
+      filename: fileName,
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.BannerPlugin({
+        banner: '#!/usr/bin/env -S node --enable-source-maps',
+        raw: true,
+      }),
+      function () {
+        this.plugin('done', () => fs.chmodSync(fileName, '755'))
+      }
+    ],
+  });
+}
+
+module.exports = {binProd: prod};
